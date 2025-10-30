c1989144578ff4ba1777afbc1bd1bb14fb110954
amoghp-tts <amogh.p@ttsystems.com>
2025-10-30 16:33:41 +0530

    Commit

--- src/app.js
+++ src/app.js
@@ -36,9 +36,7 @@ import compression from "compression";
 import morgan from "morgan";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFound } from "./middlewares/notFound.js";
-<<<<<<< Updated upstream
 import userRoutes from "./routes/user.routes.js";
-=======
 import incidentRoutes from "./routes/incident.routes.js";
 import incidentTicketRoutes from "./routes/incidentTicket.routes.js"; // Add this line
 import incidentSeverityRoutes from "./routes/incident.severity.routes.js"; // add
@@ -46,7 +44,6 @@ import incidentDSRoutes from "./routes/incidentDS.routes.js";
 import incidentViewRoutes from "./routes/incidentView.routes.js";
 import incidentHSRoutes from "./routes/incidentHS.routes.js";
 import incidentSSRoutes from "./routes/incidentSS.routes.js";
->>>>>>> Stashed changes
 
 const app = express();
 
@@ -58,17 +55,14 @@ app.use(compression());
 app.use(morgan("dev"));
 
 // Routes
-<<<<<<< Updated upstream
 app.use("/api/users", userRoutes);
-=======
 app.use("/api", incidentRoutes);
-app.use("/api", incidentTicketRoutes);
-app.use("/api", incidentSeverityRoutes);
+app.use("/api", incidentTicketRoutes); // Add this line to include incident ticket routes
+app.use("/api", incidentSeverityRoutes); // Add this line to include incident Severity routes
 app.use("/api", incidentDSRoutes);
 app.use("/api", incidentViewRoutes);
 app.use("/api", incidentHSRoutes);
 app.use("/api", incidentSSRoutes);
->>>>>>> Stashed changes
 
 // 404 handler
 app.use(notFound);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 // import dotenv from "dotenv";
 // import app from "./app.js";
 // import { connectDB } from "./config/db.js";
@@ -11,7 +9,7 @@
 
 // (async () => {
 //   try {
-//     await connectDB();
+//     await connectDB(); 
 //     app.listen(PORT, () => logger.info(`🚀 Server running on port ${PORT}`));
 //   } catch (err) {
 //     logger.error("❌ Server startup failed:", err);
@@ -19,8 +17,12 @@
 //   }
 // })();
 
+
+
+
+
+
 // server.js
->>>>>>> Stashed changes
 import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
