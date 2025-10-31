37ddd11ba5987d240f93e9728fa38cd18c90a520
SaiK-TMNA <sai.konchada@toyota.com>
2025-10-31 11:26:23 +0530

    Merge conflicts

--- src/server.js
+++ src/server.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-=======
-// import dotenv from "dotenv";
-// import app from "./app.js";
-// import { connectDB } from "./config/db.js";
-// import logger from "./config/logger.js";
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 5000;
-
-// (async () => {
-//   try {
-//     await connectDB();
-//     app.listen(PORT, () => logger.info(`🚀 Server running on port ${PORT}`));
-//   } catch (err) {
-//     logger.error("❌ Server startup failed:", err);
-//     process.exit(1);
-//   }
-// })();
-
-// server.js
->>>>>>> Stashed changes
 import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
@@ -32,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 (async () => {
   try {
-    await connectDB(); 
+    await connectDB();
     app.listen(PORT, () => logger.info(`🚀 Server running on port ${PORT}`));
   } catch (err) {
     logger.error("❌ Server startup failed:", err);
