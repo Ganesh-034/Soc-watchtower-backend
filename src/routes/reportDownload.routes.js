ecf67caa0ea9219119cce5b038aead0cb8b1116b
SaiK-TMNA <sai.konchada@toyota.com>
2025-12-11 23:35:30 +0530

    Encoded blob fix

--- src/routes/reportDownload.routes.js
+++ src/routes/reportDownload.routes.js
@@ -8,6 +8,7 @@ import {
   clearTestReports,
   createTestReport,
   createMultipleTestReports,
+  getDirectSasUrl,
   // createManualTestReport,
   //   advancedDatabaseDebug,
   // directQueryReports,
@@ -26,6 +27,7 @@ router.get("/sas-url/:month/:year", getReportSasUrl);
 router.get("/customer", getAvailableReportsForCustomer);
 //router.get('/debug/enhanced', enhancedDebugReports);
 // Debug and test routes
+router.get("/direct-sas/:month/:year", getDirectSasUrl);
 router.get("/debug", debugReports);
 router.get("/clear-test-reports", clearTestReports);
 router.get("/create-test-report", createTestReport);
